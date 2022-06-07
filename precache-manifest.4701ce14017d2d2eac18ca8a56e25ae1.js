db7d69f8e0c9b63870ee62c7f2e86dd44f0eca30
DESKTOP-0RK5E38\Orlando Nunez <nunez.orlando@aspiresoftware.com>
2022-06-07 00:00:57 -0400

    Updates

---
+++ precache-manifest.4701ce14017d2d2eac18ca8a56e25ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6e24c261f69a475179e",
+    "url": "/office-bingo/static/css/main.dce886e1.chunk.css"
+  },
+  {
+    "revision": "d6e24c261f69a475179e",
+    "url": "/office-bingo/static/js/main.d6e24c26.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/office-bingo/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "666657dab80c3ed1a80f",
+    "url": "/office-bingo/static/js/2.666657da.chunk.js"
+  },
+  {
+    "revision": "08af58468b555f234156",
+    "url": "/office-bingo/static/js/runtime~main.08af5846.js"
+  },
+  {
+    "revision": "b6d9dd57252d8ecdafe40ab413e7b8e5",
+    "url": "/office-bingo/index.html"
+  }
+];
